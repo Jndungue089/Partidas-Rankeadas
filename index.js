86d80afb14dc90ad027e69e3c65106147a4845f7
Jndungue089 <68353090+Jndungue089@users.noreply.github.com>
2024-09-26 11:12:20 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function calcularRanking(numVitorias, numDerrotas){
+    resultado = numVitorias - numDerrotas;
+    if(resultado <= 10){
+        console.log("Ferro");
+    } else if (resultado >10 && resultado <= 20){
+        console.log("Bronze");
+    } else if (20 < resultado <=50){
+        console.log("Prata");
+    } else if (50 < resultado <= 80){
+        console.log("Ouro");
+    } else if (80 < resultado <= 90) {
+        console.log("Diamante");
+    } else if (90< resultado <= 100){
+        console.log("Lendário");
+    } else if (resultado >100){
+        console.log("Imortal");
+    }
+}
+
+rl.question('Digite o número de vitórias: ', (vitorias) => {
+    rl.question('Digite o número de derrotas: ', (derrotas) => {
+        calcularRanking(parseInt(vitorias), parseInt(derrotas));
+        rl.close();
+    });
+});
